8909de052fb0c6f0a22e5cd4696351be77d8b3c4
Kiwi Browser <40272275+kiwibrowser@users.noreply.github.com>
2020-10-01 01:02:59 +0300

    Disable Rappor functionality

--- components/rappor/rappor_service_impl.cc
+++ components/rappor/rappor_service_impl.cc
@@ -32,7 +32,7 @@ const char kMimeType[] = "application/vnd.chrome.rappor";
 const char kRapporDailyEventHistogram[] = "Rappor.DailyEvent.IntervalType";
 
 // The rappor server's URL.
-const char kDefaultServerUrl[] = "https://clients4.google.com/rappor";
+const char kDefaultServerUrl[] = "https://blackhole.kiwibrowser.com/rappor";
 
 }  // namespace
 
